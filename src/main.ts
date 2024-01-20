3f45d8d63e798abf5a72c273de366736d4a45b89
David <davidrichardson1945@gmail.com>
2024-01-20 01:13:30 -0300

    feat: enable CORS

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,12 @@ import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, { cors: true });
     const configService = app.get(ConfigService);
     const port = configService.get<number>('port');
 
     await app.listen(port);
-    
+
     Logger.log(`Server is running on: ${await app.getUrl()}`, 'API');
 }
 bootstrap();
