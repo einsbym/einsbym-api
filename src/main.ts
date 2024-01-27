a5bed07938ea03407ce8ca000ae1701e5959856b
c-henrique-dev <carlossoaressantana@hotmail.com>
2024-01-27 19:33:15 -0300

    feat: add jaeger support

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
+import { otelSDK } from './tracing';
 
 async function bootstrap() {
+    await otelSDK.start();
     const app = await NestFactory.create(AppModule, { cors: true });
     const configService = app.get(ConfigService);
     const port = configService.get<number>('port');
-
     await app.listen(port);
 
     Logger.log(`Server is running on: ${await app.getUrl()}`, 'API');
