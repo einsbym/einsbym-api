c51557eeb46ebea225ef5e8f3d06a805e622bc86
David <davidrichardson1945@gmail.com>
2024-04-11 23:52:23 -0300

    refactor: disable CORS config

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, { cors: true });
+    const app = await NestFactory.create(AppModule);
     const configService = app.get(ConfigService);
     const port = configService.get<number>('port');
 
