536c170ab2c54935c909427e1b74425c397c3c79
c-henrique-dev <carlossoaressantana@hotmail.com>
2024-04-24 17:15:44 -0300

    refactor: removing class validator annotations

--- src/models/dtos/create-blog.input.ts
+++ src/models/dtos/create-blog.input.ts
@@ -1,11 +1,9 @@
 import { Field, InputType } from '@nestjs/graphql';
-import { IsNotEmpty } from 'class-validator';
 import { CreateTagInput } from './create-tag.input';
 
 @InputType()
 export class CreateBlogInput {
     @Field(() => String)
-    @IsNotEmpty({ message: 'Field title cannot be empty' })
     title: string;
 
     @Field(() => String)
