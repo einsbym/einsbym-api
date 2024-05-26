adee35ffdbc3f9294089c45206e33f9cd2ddecc9
David Silva <davidrichardson1945@gmail.com>
2024-05-26 19:40:26 -0300

    refactor(blog.resolver): remove guards from public endpoints

--- src/resolvers/blog.resolver.ts
+++ src/resolvers/blog.resolver.ts
@@ -1,6 +1,4 @@
-import { UseGuards } from '@nestjs/common';
 import { Args, Query, Resolver } from '@nestjs/graphql';
-import { JwtAuthGuard } from 'src/auth/auth.guard';
 import { Blog } from 'src/entities/blog.entity';
 import { BlogService } from 'src/providers/blog.service';
 import { File } from '../entities/file.entity';
@@ -9,13 +7,11 @@ import { File } from '../entities/file.entity';
 export class BlogResolver {
     constructor(private readonly blogService: BlogService) {}
 
-    @UseGuards(JwtAuthGuard)
     @Query(() => [Blog])
     findBlogPosts() {
         return this.blogService.find();
     }
 
-    @UseGuards(JwtAuthGuard)
     @Query(() => Blog)
     findBlogPost(@Args('id') id: string) {
         return this.blogService.findById(id);
